640a70142abf58a46d94b2daa89b1dfecacafb5d
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-09-15 14:26:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+
+
+
+let statement = "You are using " + navigator.appName +  " version " + navigator.appVersion + navigator.userAgent
+document.getElementById("browser-info").textContent = statement;
